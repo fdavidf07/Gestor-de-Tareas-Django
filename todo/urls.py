506562d79c6709f95055cc6a493d707abf3b2b1f
fdavidf07 <115109319+fdavidf07@users.noreply.github.com>
2026-05-31 10:20:13 +0200

    Mejora Gestor Tareas

--- todo/urls.py
+++ todo/urls.py
@@ -2,11 +2,15 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('accounts/', include('django.contrib.auth.urls')), # Login y Logout automáticos
+    # Cuentas y registro
+    path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
-    path('lists/delete/<int:list_id>/', views.delete_list, name='delete_list'),
+    
+    # 1. Listas de tareas
+    path('lists/', views.lists_root, name='lists_root'), 
+    path('lists/<int:list_id>/', views.list_detail_api, name='list_detail_api'),
+    
+    # 2. Tareas dentro de una lista
     path('lists/<int:list_id>/tasks/', views.task_detail, name='task_detail'),
-    path('tasks/toggle/<int:task_id>/', views.toggle_task, name='toggle_task'),
-    path('tasks/delete/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('lists/<int:list_id>/tasks/<int:task_id>/', views.task_actions_api, name='task_actions_api'),
 ]
